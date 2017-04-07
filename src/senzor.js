e8f1da1b3604dee5bd2c33969cd7ee8201710da5
SuciuFlaviu <flaviusuciuflorian@gmail.com>
2017-04-07 18:44:22 +0300

    Desenat dereptunghi alpha

---
+++ src/senzor.js
@@ -0,0 +1,31 @@
+
+
+
+
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(evt)
+{
+	var alpha = evt.alpha;
+	var beta = evt.beta;
+	var gamma = evt.gamma;
+	
+	document.getElementById("a").innerHTML = "alpha = " + alpha.toString();
+	document.getElementById("b").innerHTML = "beta = " + beta.toString();
+	document.getElementById("g").innerHTML = "gamma = " + gamma.toString();
+	
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	var raza = 20;
+	var centru = {x:canvas.width / 2, y:canvas.height / 2};
+	var latura_patrat = 50;
+	
+	
+	ctx.beginPath();
+	//ctx.arc(centru.x + gamma * (canvas.width / 2 - raza) / 90, centru.y + beta * (canvas.height / 2 - raza) / 90, raza, 0, 2 * Math.PI);
+	ctx.rect(centru.x - latura_patrat/2,centru.y - latura_patrat/2,latura_patrat,latura_patrat);
+	ctx.stroke();
+}
